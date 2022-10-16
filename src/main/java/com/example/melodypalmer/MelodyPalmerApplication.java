7cc0ecec7e62d809aa2304ebd8e2b9c0ee678da8
modelkit62 <modelkit62@gmail.com>
2022-10-16 13:02:14 +0100

    Initial commit

---
+++ src/main/java/com/example/melodypalmer/MelodyPalmerApplication.java
@@ -0,0 +1,13 @@
+package com.example.melodypalmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyPalmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyPalmerApplication.class, args);
+	}
+
+}
